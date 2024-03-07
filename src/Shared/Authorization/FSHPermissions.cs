0925f55fd5bbaa16b6198b65c84ec6c7545e2944
Kevin-Joel Agbeto <agbetokevin@gmail.com>
2024-03-07 09:43:03 +0000

    setting up client config to use the TypeChambre endpoints and the component TypeChampes

--- src/Shared/Authorization/FSHPermissions.cs
+++ src/Shared/Authorization/FSHPermissions.cs
@@ -25,6 +25,7 @@ public static class FSHResource
     public const string RoleClaims = nameof(RoleClaims);
     public const string Products = nameof(Products);
     public const string Brands = nameof(Brands);
+    public const string TypeChambres = nameof(TypeChambres);
 }
 
 public static class FSHPermissions
@@ -60,6 +61,10 @@ public static class FSHPermissions
         new("Delete Brands", FSHAction.Delete, FSHResource.Brands),
         new("Generate Brands", FSHAction.Generate, FSHResource.Brands),
         new("Clean Brands", FSHAction.Clean, FSHResource.Brands),
+        new("Create TypeChambres", FSHAction.Create, FSHResource.TypeChambres),
+        new("Search TypeChambres", FSHAction.View, FSHResource.TypeChambres),
+        new("Update TypeChambres", FSHAction.Update, FSHResource.TypeChambres),
+        new("Delete TypeChambres", FSHAction.Delete, FSHResource.TypeChambres),
         new("View Tenants", FSHAction.View, FSHResource.Tenants, IsRoot: true),
         new("Create Tenants", FSHAction.Create, FSHResource.Tenants, IsRoot: true),
         new("Update Tenants", FSHAction.Update, FSHResource.Tenants, IsRoot: true),
